453f1f19c55e6d89303ef0469d7ddca4bd55fa69
hero <antonstendal19@gmail.com>
2026-04-20 18:52:55 +0200

    Add JOIN FETCH to OrderDao#getByUser HQL query

--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -37,7 +37,9 @@ public class OrderDaoImpl implements OrderDao {
     @Override
     public List<Order> getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            return session.createQuery("FROM Order o where o.user = :user",
+            return session.createQuery("SELECT DISTINCT o FROM Order o "
+                            + "JOIN FETCH o.tickets "
+                            + "WHERE o.user = :user",
                     Order.class).setParameter("user", user).getResultList();
         } catch (Exception e) {
             throw new DataProcessingException("Can't find order history by user: " + user, e);
